8ce89bbb938468f6fe33b6efd5c38e53cf76375d
some-earth11 <sk8762@srmist.edu.in>
2023-09-12 10:17:53 +0530

    updated packages, added routes

--- server/controllers/documentController.js
+++ server/controllers/documentController.js
@@ -1,19 +1,55 @@
+const openai = require('openai');
 const Document = require('../models/Document');
 
-exports.uploadDocument = async (req, res) => {
-  const { title, content } = req.body;
+const OpenAIAPI = new openai.OpenAIAPI({
+  key: process.env.OPENAI_API_KEY,
+});
 
-  const document = new Document({
-    title,
-    content,
-    owner: req.user._id,
-  });
+const document = new Document();
 
-  await document.save();
-  res.redirect('/dashboard');
+exports.uploadDocument = (req, res) => {
+  const { file } = req.body;
+  const documentText = file;
+
+  OpenAIAPI.complete({
+    engine: 'davinci',
+    prompt: documentText,
+    max_tokens: 150,
+    temperature: 0.7,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  })
+    .then((response) => {
+      document.setSummary(response.choices[0].text.trim());
+      document.queries = [];
+      res.json({ message: 'Document processed successfully' });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: 'An error occurred while processing the document' });
+    });
 };
 
-exports.getDocument = async (req, res) => {
-  const document = await Document.findById(req.params.id);
-  res.json(document);
+exports.queryDocument = (req, res) => {
+  const { query } = req.body;
+
+  OpenAIAPI.complete({
+    engine: 'davinci',
+    prompt: `Document Summary: ${document.summary}\nQuery: ${query}\n`,
+    max_tokens: 50,
+    temperature: 0.7,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  })
+    .then((response) => {
+      const queryResponse = response.choices[0].text.trim();
+      document.addQuery(query, queryResponse);
+      res.json({ response: queryResponse });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: 'An error occurred while processing the query' });
+    });
 };
--- server/index.js
+++ server/index.js
@@ -1,34 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const session = require('express-session');
-const passport = require('passport');
+const dotenv = require('dotenv');
+const documentRoutes = require('./routes/documentRoutes');
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/legal_document_website', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/documents', documentRoutes); // Mount documentRoutes to '/documents' path
 
-app.use(session({
-  secret: 'your-secret-key', // Change this to a secure secret
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore({ mongooseConnection: mongoose.connection }),
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Load routes
-app.use('/auth', require('./routes/authRoutes'));
-app.use('/documents', require('./routes/documentRoutes'));
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
